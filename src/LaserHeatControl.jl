539cb29dd7e710642cf68efe0de78ae425d411ce
urlicht <jungsu93@gmail.com>
2021-05-20 11:28:28 -0400

    add model fitting; modify module file

--- src/LaserHeatControl.jl
+++ src/LaserHeatControl.jl
@@ -2,10 +2,19 @@ module LaserHeatControl
 
 using NIDAQ, PyPlot, Optim, DSP, Statistics, ProgressMeter
 
+printstyled("WARNING: risk of permanant eye damamge. 
+Using this class 4 laser requires training and approval."; color = :red)
+
 include("constant.jl")
 include("model_fit.jl")
 include("nidaq.jl")
 include("pulse_meas.jl")
 include("thermistor.jl")
+
+# pulse_meas.jl
+export pulse_model_fit,
+    pulse_align,
+    # model_fit.jl
+    fit_laser_model
 
 end # module
--- src/model_fit.jl
+++ src/model_fit.jl
@@ -1,9 +1,11 @@
 function fit_laser_model(list_U, list_v, list_t; plot_model=true)
+    # detect heat on period
     idx_start = findfirst(list_v .> 1.1)
     idx_end = findlast(list_v .> 1.1)
-    
+
     U0 = mean(list_U[1:5000])
-    
+    list_U_heat = list_U[idx_start:idx_end]
+    list_t_heat = collect(0:length(list_U_heat)-1) ./ 1000;
     list_U_cool = list_U[idx_end:end] .- U0
     U_max_cool = maximum(list_U_cool)
     list_U_cool_norm = list_U_cool / U_max_cool
@@ -16,14 +18,26 @@ function fit_laser_model(list_U, list_v, list_t; plot_model=true)
 
     β = (optim_cool.minimizer[1])
     τ = log(2) / β
-    if plot_model
-        figure(figsize=(4,4))
-        plot(list_t_cooling, data_cooling_norm, label="data")
-        plot(list_t_cooling, model_cooling(list_t_cooling, optim_cool.minimizer), label="model")
-        xlabel("t (s)")
-        ylabel("U (norm)")
-        legend()
-        tight_layout()
+    
+    α = let 
+        dt = length(idx_start:idx_end) / 1000
+        dT = list_U[idx_end] - list_U[idx_start]
+        (dT + dt * β * list_U[idx_start]) / (dt * 2)
     end
     
+    if plot_model
+#         figure(figsize=(4,4))
+#         plot(list_t_cooling, data_cooling_norm, label="data")
+#         plot(list_t_cooling, model_cooling(list_t_cooling, optim_cool.minimizer), label="model")
+#         xlabel("t (s)")
+#         ylabel("U (norm)")
+#         legend()
+#         tight_layout()
+    end
+
+    println("α = $(α) deg C / (A s)")
+    println("β = $(β) / s")
+    println("half decay = $(log(2)/β) s")
+
+    α, β, τ
 end
